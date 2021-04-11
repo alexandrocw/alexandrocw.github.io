66cc462d211a2e51afe06f2e79bdc41112d27900
lKingDGl <alexandrowijaya@yahoo.co.id>
2021-04-12 00:02:05 +0700

    Develop front-end

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+// Initialize dotenv environment
 require('dotenv').config();
 
+
+// Import required frameworks and modules
 const express = require('express');
 const app = express();
 const config = require('./config');
 const path = require('path');
 
+// For app to use assets folder
 app.use("/assets", express.static(path.join(__dirname + "/assets")));
 
+// GET response
 app.get("", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+
+// Listen to port
 app.listen(config.PORT, () => {
     console.log(`Listening on PORT ${config.PORT}`);
 });
